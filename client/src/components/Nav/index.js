3cf6fa5ae710d68b25bcd885070e4642fd49a55d
Hernavch <hernandezvc1292@gmail.com>
2019-05-08 21:47:26 -0700

    Nav Bar

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -6,6 +6,16 @@ function Nav() {
       <a className="navbar-brand" href="/">
         React Reading List
       </a>
+      <ul className="navbar-nav mr-auto mt-2 mt-lg-0">
+          <li className="nav-item active">
+            <a className="nav-link" href="#"
+              >Home<span className="sr-only">(current)</span></a
+            >
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Update Profile</a>
+          </li>
+        </ul>
     </nav>
   );
 }
